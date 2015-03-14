ad21c064041200c7da1d4918ed10c4533bf3a51a
Joe Eaves <jinux@alluha.net>
2015-03-14 18:33:31 +0000

    Added puppet repo install

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 main() {
-	printMessage "Running Puppet Manifests..."
+	printMessage "Configuring base system..."
+	baseSystem
+	printMessage "Running Puppet manifests..."
 	executeScripts .
 	printMessage "Complete."
 }
@@ -10,16 +12,23 @@ printMessage() {
 	echo "[KunInst] $1"
 }
 
+baseSystem() {
+	printMessage " - Installing Puppet repository..."
+	rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
+	printMessage " - Done"
+}
+
 executeScripts() {
 	local dir="$1"
 	for puppetFile in $(ls $dir); do
-		printMessage "Applying $puppetFile..."
+		printMessage " - Applying $puppetFile..."
 		puppet apply "$puppetFile"
 		if [[ $? -ne 0 ]]; then
 			printMessage "There was an error, exiting!"
 			exit 1
 		fi
 	done
+	printMessage " - Done"
 }
 
 main
