ab130565ba0ad2cbb96a077065a121c570e37383
Joe Eaves <jinux@alluha.net>
2015-04-19 14:34:39 +0100

    dldns::record syntax error with the hash assignment

--- puppet/dldns/manifests/record.pp
+++ puppet/dldns/manifests/record.pp
@@ -7,17 +7,23 @@ define dldns::record (
 	validate_hash($record)
 	validate_string($record[recordname])
 	$recordname_sha = sha1($record[recordname])
-	record[title] = $title
+	new_record = {
+		title 	   => $title,
+		zone       => $record[zone],
+	    ttl        => $record[ttl],
+	    localname  => $record[localname],
+	    recordname => $record[recordname]
+	}
 	if ($provider == 'r53') {
 		$rtitle = "R53 Record - ${recordname_sha}"
 		if (masterless) {
 			::dldns::r53u {$rtitle:
-				record      => $record,
+				record      => $new_record,
 				call_binary => true
 			}
 		} else {
 			@@::dldns::r53u {$rtitle:
-				record => $record
+				record => $new_record
 			}
 		}
 	} else {
