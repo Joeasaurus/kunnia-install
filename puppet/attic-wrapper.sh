9c4d0052bf8f861f30ea01a3e84543e3ebf08bb4
Joe Eaves <jinux@alluha.net>
2015-04-25 12:42:00 +0100

    Added lock file to wait on concurrent connections

--- puppet/attic-wrapper.sh
+++ puppet/attic-wrapper.sh
@@ -1,17 +1,28 @@
 #!/usr/bin/env bash
 
+LOCK_FILE="$HOME/.attic"
+exit_clean() {
+	echo "$1"
+	rm -rf "$LOCK_FILE"
+	exit $2
+}
+while [[ -f "$LOCK_FILE" ]]; do
+	sleep 2
+done
+touch "$LOCK_FILE"
+
 AWS_BUCKET="cloud.kunniagaming.net"
 AWS_COMMAND="/usr/local/bin/aws --region $(/usr/local/bin/aws s3api get-bucket-location --output text --bucket $AWS_BUCKET)"
 
 COMMAND="$1"
-[[ "$COMMAND" != "attic" ]] && echo "Wrong command" && exit 5
+[[ "$COMMAND" != "attic" ]] && exit_clean "Wrong command" 5
 shift
 
 /usr/local/bin/attic $@
 if [[ $? -eq 0 ]]; then
 	"$AWS_COMMAND" s3 sync "$HOME" "s3://$AWS_BUCKET/$USER"
-	[[ $? -ne 0 ]] && echo "!! FAILED TO SYNC TO S3 !!" && exit 2
+	[[ $? -ne 0 ]] && exit_clean "!! FAILED TO SYNC TO S3 !!" 2
 else
-	echo "!! FAILED TO RUN ATTIC !!"
-	exit 1
+	exit_clean "!! FAILED TO RUN ATTIC !!" 1
 fi
+exit_clean "Complete!" 0
