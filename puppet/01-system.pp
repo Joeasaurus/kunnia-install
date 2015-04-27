6f09a33e9c6413b3ad5a50d05232c9507ab3b932
Joe Eaves <jinux@alluha.net>
2015-04-27 21:38:59 +0100

    Added extra domain records

--- puppet/01-system.pp
+++ puppet/01-system.pp
@@ -17,7 +17,7 @@ rsyslog::snippet {'10-httpd':
 ## DNS
 class {'::dldns::install':
   source => "/etc/puppet/modules/dldns/files/bin/r53u"
-} ->
+}
 ::dldns::record {'BaseRecord':
   masterless => true,
   provider   => 'r53',
@@ -25,8 +25,31 @@ class {'::dldns::install':
     zone       => 'cloud.kunniagaming.net',
     ttl        => '300',
     localname  => $::ec2_public_hostname,
-    recordname => "c-1"
-  }
+    recordname => 'c-1'
+  },
+  require    => Class['::dldns::install']
+}
+::dldns::record {'MumbleRecord':
+  masterless => true,
+  provider   => 'r53',
+  record     => {
+    zone       => 'cloud.kunniagaming.net',
+    ttl        => '300',
+    localname  => 'c-1.cloud.kunniagaming.net',
+    recordname => 'mumble'
+  },
+  require    => Class['::dldns::install']
+}
+::dldns::record {'MumbleRecord':
+  masterless => true,
+  provider   => 'r53',
+  record     => {
+    zone       => 'cloud.kunniagaming.net',
+    ttl        => '300',
+    localname  => 'c-1.cloud.kunniagaming.net',
+    recordname => 'backup'
+  },
+  require    => Class['::dldns::install']
 }
 
 ## Tools
